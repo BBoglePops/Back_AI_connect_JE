21c9a6cd3e09f874043789af099ec052d86dae04
Kim JungEun <grace0130kim@gmail.com>
2024-05-25 03:00:53 +0900

    Feat: eyetrack feedback

--- Eyetrack/models.py
+++ Eyetrack/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class GazeTrackingResult(models.Model):
     #image = models.ImageField(upload_to='gaze_images/')
     encoded_image = models.TextField()
+    feedback = models.TextField(default="No feedback provided.") 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
